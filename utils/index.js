0da7081b54a4cabe0de85bc9ba71ff408cf45364
Tyler Twining <twijn@twijn.net>
2023-12-02 13:26:30 -0500

    Add comma(num) function to utils

--- utils/index.js
+++ utils/index.js
@@ -21,6 +21,17 @@ class Utils {
         return str;
     }
 
+    /**
+     * Converts a number into a string with commas
+     * Example: 130456 -> 130,456
+     * @param {number} num 
+     * @returns {string}
+     */
+    comma(num) {
+        if (!num) return "0";
+        return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+
 }
 
 module.exports = new Utils();
